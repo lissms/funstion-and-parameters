23c04697475a1fcccba15a4957869cca08e48dc6
lissms <felissms@gmail.com>
2020-10-18 18:27:39 +0200

    add phase-4

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 "use strict";
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
 
 const writeThis = (word, number) => {
-  for (let i = 0; i < number; i++) {
-    console.log(word);
-  }
+  for (let i = 0; i < number; i++) {}
+  console.log("version uno:", word, number);
 };
-writeThis("patata", 10);
-writeThis("Aguacate", 7);
-writeThis("Pizza", 50);
+writeThis("patata", getRandomNumber(10));
+writeThis("Aguacate", getRandomNumber(10));
+writeThis("Pizza", getRandomNumber(10));
+
+/* aqui tendo dudas si patata, aguacate y pizza 
+deberian coger el mismo numero random. En este 
+caso el segundo parametro deberia ser otro, porq 
+ cada vez que llamo a la funcion, ella saca un 
+ numero nuevo. Por eso no es la misma cantidad de
+  patata, aguacate y pizza  */
+
+/* version dos: Quiero que me salgan la misma cantidad de veces */
+
+function getRandomNumber(max) {
+  return Math.ceil(Math.random() * max);
+}
+let numberRandom = getRandomNumber(10);
+
+const writeThisV2 = (word, number) => {
+  for (let i = 0; i < number; i++) {}
+  console.log("version dos del ejercicio:", word, number);
+};
+writeThisV2("patata", numberRandom);
+writeThisV2("Aguacate", numberRandom);
+writeThisV2("Pizza", numberRandom);
